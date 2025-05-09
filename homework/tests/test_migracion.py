53d3f3889418f74603ba37a70761066fd77a048d
Valentamayog <vatamayog@unal.edu.co>
2025-05-09 12:05:59 -0500

    Solución tarea 2
    - Creación de Función Wordcount y test de migración
    - Creación de funciones en internals

---
+++ homework/tests/test_migracion.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+from ..src.wordcount import main 
+
+
+def test_migracion():
+
+    try:
+        subprocess.run(
+            ["python3", "-m", "homework", "data/input", "data/output"],
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        raise Exception(f"Error running the homework script: {e}")
+
+    if not os.path.exists("data/output/wordcount.tsv"):
+        raise FileNotFoundError("El archivo wordcount.tsv no existe.")
+
+    results = {}
+    with open("data/output/wordcount.tsv", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    for line in lines:
+        key, value = line.strip().split("\t")
+        results[key] = value
+
+    assert results.get("computational", 0) == "3"
+    assert results.get("analytics", 0) == "5"
